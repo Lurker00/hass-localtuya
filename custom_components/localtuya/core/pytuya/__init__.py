19000e25c3fbde7e9f62efd1451f1ff04a83f43e
Lurker00 <kis@aha.ru>
2024-10-03 14:34:13 +0300

    Wrong condition

--- custom_components/localtuya/core/pytuya/__init__.py
+++ custom_components/localtuya/core/pytuya/__init__.py
@@ -1599,7 +1599,7 @@ class TuyaProtocol(asyncio.Protocol, ContextualLogger):
 
     @property
     def is_connected(self):
-        return not self.transport or not self.transport.is_closing()
+        return self.transport and not self.transport.is_closing()
 
     @property
     def last_command_sent(self):
